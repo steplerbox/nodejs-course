c0c24265cacbb83d393f45ee9405cb2f9f16855a
steplerbox <stepler.box@gmail.com>
2019-08-05 00:17:07 +0300

    added mongoose, created document

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/nodejs-course')
+  .then(() => console.log('connected to mongoDB...'))
+  .catch(err => console.error('failed connect to mongoDB...', err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [ String ],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+const createCourse = async (courseData) => {
+  const course = new Course(courseData);
+
+  const result = await course.save();
+  console.log(result);
+  return result;
+};
+
+// createCourse({
+//   name: 'Node.js course',
+//   author: 'Mosh',
+//   tags: ['nodejs', 'mongodb'],
+//   isPublished: true
+// });
+
 
 const home = require('./src/routes/home');
 const courses = require('./src/routes/courses');
