17ee3444cad3e97e0fec736299b760bcbee531f2
steplerbox <stepler.box@gmail.com>
2019-10-30 18:58:36 +0200

    fix status code

--- src/middlewares/admin.js
+++ src/middlewares/admin.js
@@ -1,7 +1,7 @@
 
 module.exports = (req, res, next) => {
   if (!req.user.isAdmin) {
-    return res.status(401).send('Access denied.');
+    return res.status(403).send('Access denied.');
   }
 
   next();
